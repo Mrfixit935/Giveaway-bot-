2adcd86eccb7be02d60e79b1779b3da7ced55fac
Mrfixit935 <61997640+Mrfixit935@users.noreply.github.com>
2022-05-22 17:15:58 -0400

    update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { Client, Collection, MessageEmbed } = require("discord.js");
-const fs = require('fs');
 const config = require("./config.json");
 const Discord = require("discord.js")
 
@@ -19,7 +18,6 @@ const prefix = require("./config.json");
 
 
 // Initializing the project
-require("./handler")(client);
 require('./Systems/GiveawaySys')(client);
 
 
@@ -52,40 +50,40 @@ client.on('ready', () => {
 );
 
 
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config.json');
+const fs = require('node:fs');
 
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+// Place your client and guild ids here
+const clientId = '967922987900739616';
+const guildId = '859095770236846120';
 
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
-client.on('guildMemberRemove', (member) => {
-  member.guild.channels.cache.get("958478336521285692").send(`${member.user} has left the server!`);
+const rest = new REST({ version: '9' }).setToken(token);
 
-})
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
 
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
 
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
 
-
-
-client.on('guildMemberAdd', (member) => {
-  		// member.guild.channels.cache.get("849283385808912384").send(`${member.user} has joined the server!`);
-      //console.log(member.user);
-
-      const newMemberEmbed = new Discord.MessageEmbed()
-        .setColor("#d81e5b")
-        .setTitle("New Member!")
-        .setDescription(`${member.user} has joined the server! We hope you enjoy your stay! ID ${member.user.id}`)
-        .setThumbnail(member.user.displayAvatarURL())
-        .setTimestamp();
-      
-  
-  
-        member.guild.channels.cache.get("958478336521285692").send({
-          embeds: [newMemberEmbed] 
-        })
-  
-
-      
-        
-    })
 
    
     
